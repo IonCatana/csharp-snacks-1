a2206c381af86079b1b1696e85a3947f0509d65b
IonCatana <catana.ion17@yahoo.it>
2022-06-27 14:59:31 +0200

    Snack 5 fatto

--- Program.cs
+++ Program.cs
@@ -11,7 +11,9 @@ Console.WriteLine($"La somma totale dei numeri inseriti é : {somma}");
 */
 
 //Snack 4
-int sum4 = 0;
+
+
+/*int sum4 = 0;
 float avg = 0f;
 int a = 0;
 
@@ -25,5 +27,26 @@ for (int i = 2; i <= 10; i++)
 avg = sum4 / a;
 Console.WriteLine(sum4);
 Console.WriteLine(avg);
+
+return;
+*/
+
+//Shack 5
+//Il software chiede all'utente di inserire un numero.
+//Se il numero inserito é pari, stampa il numero, se dispari, stampa il numero successivo.
+
+Console.Write("Inserisci un numero: ");
+int userNum1 = Int32.Parse(Console.ReadLine());
+
+if (userNum1 % 2 == 0)
+{
+    Console.WriteLine("il numero e': " + userNum1);
+}
+else
+{
+    Console.WriteLine("il numero e': " + (userNum1 + 1));
+}
+
+
 
 return;
