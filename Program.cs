31e74564d4660eae2bf24bd80af4a4a9bf0ee9f5
IonCatana <catana.ion17@yahoo.it>
2022-06-27 15:39:02 +0200

    Snack 10 fatto

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿
+
+
 // Snack 3
 /*Console.Write("Inserisci 10 numeri: ");
 int somma = 0;
@@ -9,6 +11,8 @@ for (int i = 0; i < 10; i++)
 }
 Console.WriteLine($"La somma totale dei numeri inseriti é : {somma}");
 */
+
+
 
 //Snack 4
 
@@ -31,11 +35,12 @@ Console.WriteLine(avg);
 return;
 */
 
+
 //Shack 5
 //Il software chiede all'utente di inserire un numero.
 //Se il numero inserito é pari, stampa il numero, se dispari, stampa il numero successivo.
 
-Console.Write("Inserisci un numero: ");
+/*Console.Write("Inserisci un numero: ");
 int userNum1 = Int32.Parse(Console.ReadLine());
 
 if (userNum1 % 2 == 0)
@@ -49,4 +54,42 @@ else
 
 
 
-return;
+return;*/
+
+//Snack 10
+//fai inserire un numero, che chiameremo N, all'utente.
+//Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100.
+//Ogni volta che ne crei uno, stampalo a schermo.
+{
+    Console.WriteLine($"\nInsert a number: ");
+    int N = Convert.ToInt32(Console.ReadLine());
+
+    Random rnd = new Random();
+
+    for (int i = 0; i < N; i++)
+    {
+        int[] array = new int[10];
+
+        for (int j = 0; j < array.Length; j++)
+        {
+            array[j] = rnd.Next(1, 101);
+        }
+
+        // Stampa array
+        Console.Write($"\nArray {i + 1}: [");
+        for (int j = 0; j < array.Length; j++)
+        {
+            Console.Write($"{array[j]}");
+            if (j < array.Length - 1)
+            {
+                Console.Write(",\t");
+            }
+        }
+        Console.Write("]");
+    }
+
+    Console.WriteLine();
+
+    return;
+}
+
