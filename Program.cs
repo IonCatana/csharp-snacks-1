905205dd43a4feb40c8493327abbec80b883ba8d
IonCatana <catana.ion17@yahoo.it>
2022-06-27 14:54:24 +0200

    snack 4 fatto

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿
-Console.Write("Inserisci 10 numeri: ");
+// Snack 3
+/*Console.Write("Inserisci 10 numeri: ");
 int somma = 0;
 for (int i = 0; i < 10; i++)
 {
@@ -7,3 +8,22 @@ for (int i = 0; i < 10; i++)
     somma += numeroutente;
 }
 Console.WriteLine($"La somma totale dei numeri inseriti é : {somma}");
+*/
+
+//Snack 4
+int sum4 = 0;
+float avg = 0f;
+int a = 0;
+
+for (int i = 2; i <= 10; i++)
+{
+    a += 1;
+    sum4 = sum4 + i;
+
+}
+
+avg = sum4 / a;
+Console.WriteLine(sum4);
+Console.WriteLine(avg);
+
+return;
