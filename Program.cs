a6313379a8e9d03d527bcb6e51f3f6edc8a5ddf3
IonCatana <catana.ion17@yahoo.it>
2022-06-27 14:36:53 +0200

    snack 3 fatto

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Write("Inserisci 10 numeri: ");
+int somma = 0;
+for (int i = 0; i < 10; i++)
+{
+    int numeroutente = int.Parse(Console.ReadLine());
+    somma += numeroutente;
+}
+Console.WriteLine($"La somma totale dei numeri inseriti é : {somma}");
